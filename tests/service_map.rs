85db798802dab834024dac7ed79c78b5fd2ecc67
Naja Melan <najamelan@autistici.org>
2019-05-13 22:26:28 +0200

    Unit test for servicemap debug impl

--- tests/service_map.rs
+++ tests/service_map.rs
@@ -1,5 +1,14 @@
 #![ feature( async_await, arbitrary_self_types, specialization, nll, never_type, unboxed_closures, trait_alias, box_syntax, box_patterns, todo_macro, try_trait, optin_builtin_traits ) ]
 
+// Tested:
+//
+// - ✔ Verify that the same      service, in a   different namespace has different service id.
+// - ✔ Verify that a   different service, in the same      namespace has different service id.
+// - ✔ Verify that the same      service, in the same      namespace but in different servicemap has identical sid
+// - ✔ Test clone.
+// - ✔ Test Debug.
+//
+
 
 mod common;
 
@@ -52,7 +61,7 @@ fn sid_diff_for_diff_ns()
 }
 
 
-// Verify that the same service, in a different namespace has different service id.
+// Verify that a different service, in the same namespace has different service id.
 //
 #[ test ]
 //
@@ -62,7 +71,7 @@ fn sid_diff_for_diff_service()
 }
 
 
-// Verify that the same service in different service maps with the same namespace has identical sid
+// Verify that the same service in different servicemaps with the same namespace has identical sid
 //
 #[ test ]
 //
@@ -95,4 +104,39 @@ fn clone()
 
 	assert_eq!( serial, format!( "{:?}", sm.clone() ) );
 }
+
+
+
+// Test debug implementation.
+// Fixes the output of the debug implementation. Mainly, this fixes the sid impl. If sid's change,
+// that would be a breaking change, because people might be counting on them, especially if there
+// would be programs written in other languages, because those would manually implement the wire protocol.
+// Thus if this changes, you should bump a breaking change version.
+//
+#[test]
+//
+fn debug()
+{
+	// Create mailbox for our handler
+	//
+	let addr_handler = Addr::try_from( Sum(0) ).expect( "spawn actor mailbox" );
+
+	// Create a service map
+	//
+	let mut sm = remotes::Services::new();
+	// Register our handlers
+	//
+	sm.register_handler::<Add >( Receiver::new( addr_handler.recipient() ) );
+	sm.register_handler::<Show>( Receiver::new( addr_handler.recipient() ) );
+
+	let txt =
+"remotes::Services
+{
+	Add  - sid: c5c22cab4f2d334e0000000000000000 - handler (actor_id): 0
+	Show - sid: 0617b1cfb55b99700000000000000000 - handler (actor_id): 0
+}";
+
+	assert_eq!( txt, format!( "{:?}", sm ) );
+}
+
 
